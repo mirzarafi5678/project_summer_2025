e9e0ebcc0139fcad5520376dd6340b8a9c63b9a1
Isratjahan1765 <mirzasufianrafi5678@gmail.com>
2025-08-06 23:24:43 +0600

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,16 +2,13 @@ module com.example.finalproject_oop213 {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.finalproject_oop213 to java.base, javafx.fxml;
     opens com.example.finalproject_oop213.MirzaMdSufianHridoy to javafx.fxml,java.base;
+    opens com.example.finalproject_oop213.IsratJahan to javafx.fxml,java.base;
     opens com.example.finalproject_oop213.MirzaMdSufianHridoy.PassengerController to java.base, javafx.fxml;
     opens com.example.finalproject_oop213.MirzaMdSufianHridoy.TicketAgentController to java.base, javafx.fxml;
-
     opens com.example.finalproject_oop213.IsratJahanBithi to javafx.fxml,java.base;
     opens com.example.finalproject_oop213.ArifFaisal to javafx.fxml,java.base;
     opens com.example.finalproject_oop213.TouhidYash to javafx.fxml,java.base;
     exports com.example.finalproject_oop213;
-
-
 }
