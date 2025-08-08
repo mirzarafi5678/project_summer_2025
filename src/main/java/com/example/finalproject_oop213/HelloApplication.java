85d9f060f486fb74f76b51e4b9553ce114bdf840
mirzarafi5678 <2310360@iub.edu.bd>
2025-08-08 21:52:34 +0600

    Ticket Agent part is just started

--- src/main/java/com/example/finalproject_oop213/HelloApplication.java
+++ src/main/java/com/example/finalproject_oop213/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/Passenger-Dashboard-page.fxml"));
+                HelloApplication.class.getResource("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/TicketAgent-Dashboard-page.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load(), 720, 740);
         stage.setTitle("Hello!");
--- src/main/java/com/example/finalproject_oop213/MirzaMdSufianHridoy/TicketAgentController/TicketAgentDashBoardController.java
+++ src/main/java/com/example/finalproject_oop213/MirzaMdSufianHridoy/TicketAgentController/TicketAgentDashBoardController.java
@@ -1,50 +1,62 @@
 package com.example.finalproject_oop213.MirzaMdSufianHridoy.TicketAgentController;
 
+import com.example.finalproject_oop213.MirzaMdSufianHridoy.SceneSwitcher;
 import javafx.event.ActionEvent;
+
+import java.io.IOException;
 
 public class TicketAgentDashBoardController {
     @javafx.fxml.FXML
-    public void AddNoticeButton(ActionEvent actionEvent) {
-    }
+    public void AddNoticeButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/addNoticeByAgent-page.fxml",actionEvent);
 
-    @javafx.fxml.FXML
-    public void SeeBookingSummeryButton(ActionEvent actionEvent) {
-    }
-
-
-
-    @javafx.fxml.FXML
-    public void AddLunchTripButton(ActionEvent actionEvent) {
-    }
-
-
-
-
-    @javafx.fxml.FXML
-    public void CraetApassengerTicketButton(ActionEvent actionEvent) {
     }
 
 
 
     @javafx.fxml.FXML
-    public void SeeAllPassengerCargoSpace(ActionEvent actionEvent) {
+    public void AddLunchTripButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/removeOrAddLaunchTrip-page.fxml",actionEvent);
+    }
+
+
+
+
+    @javafx.fxml.FXML
+    public void CraetApassengerTicketButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/createPassengerTicket-page.fxml",actionEvent);
+    }
+
+
+
+    @javafx.fxml.FXML
+    public void SeeAllPassengerCargoSpace(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/SeeAllCargoSpaceInfo-Page.fxml",actionEvent);
+
     }
 
     @javafx.fxml.FXML
-    public void DownlaodAllPassegerDetail(ActionEvent actionEvent) {
+    public void DownlaodAllPassegerDetail(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/DownloadAllPassengerTicketDetail-Page.fxml");
     }
 
     @javafx.fxml.FXML
-    public void ViewAllPassengerFromDatabaseButton(ActionEvent actionEvent) {
+    public void ViewAllPassengerFromDatabaseButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/viewAllPassengerFromDatabase-page.fxml",actionEvent);
     }
 
     @javafx.fxml.FXML
-    public void RemovePassengerTicket(ActionEvent actionEvent) {
+    public void RemovePassengerTicket(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/RemovePassengerTicket-Pgae.fxml",actionEvent);
     }
     @javafx.fxml.FXML
-    public void LogOutButton(ActionEvent actionEvent) {
+    public void LogOutButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/login-page.fxml",actionEvent);
     }
 
 
-
+    @javafx.fxml.FXML
+    public void CalculatePriceButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/CalculateTotalPrice-page.fxml");
+    }
 }
