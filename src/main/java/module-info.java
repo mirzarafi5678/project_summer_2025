857a03737dd4041f546029b68c5cc523328dff7f
mirzarafi5678 <2310360@iub.edu.bd>
2025-08-08 19:37:01 +0600

    passenger part done

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,7 @@ module com.example.finalproject_oop {
     opens com.example.finalproject_oop213.IsratJahan to javafx.fxml, java.base;
     opens com.example.finalproject_oop213.MirzaMdSufianHridoy.PassengerController to java.base, javafx.fxml;
     opens com.example.finalproject_oop213.MirzaMdSufianHridoy.TicketAgentController to java.base, javafx.fxml;
-    opens com.example.finalproject_oop213.IsratJahanBithi to javafx.fxml, java.base;
+
     opens com.example.finalproject_oop213.ArifFaisal to javafx.fxml, java.base;
     opens com.example.finalproject_oop213.TouhidYash to javafx.fxml, java.base;
 
