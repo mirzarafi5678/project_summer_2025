3223e9ea9b8a7f5cba1bb5e02d47a3b90d0e1954
Isratjahan1765 <mirzasufianrafi5678@gmail.com>
2025-08-08 16:37:31 +0600

    Updated IsratJahan module with new controllers and FXML changes

---
+++ src/main/java/com/example/finalproject_oop213/IsratJahan/CalculateProfitController.java
@@ -0,0 +1,56 @@
+package com.example.finalproject_oop213.IsratJahan;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CalculateProfitController {
+
+    @FXML
+    private TableColumn<?, ?> addAmountColumn;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    private Label depositLabel;
+
+    @FXML
+    private Label expenseLabal;
+
+    @FXML
+    private TableColumn<?, ?> investAmountColumn;
+
+    @FXML
+    private TableColumn<?, ?> profitColumn;
+
+    @FXML
+    private TableColumn<?, ?> purposeColumn;
+
+    @FXML
+    private TableColumn<?, ?> revenueColumn;
+
+    @FXML
+    private Label revenueLabel;
+
+    @FXML
+    private TableView<?> tableView;
+
+    @FXML
+    void BackButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void calculateButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize(){
+
+    }
+
+}
