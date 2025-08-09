65f242dfc00f15b0e8b46a0f4021526a6aed3700
Isratjahan1765 <esratjahan1765@gmail.com>
2025-08-09 13:13:16 +0600

    Added and updated IsratJahan module controllers and FXML files for Agent and Financial Officer dashboards

--- src/main/java/com/example/finalproject_oop213/HelloApplication.java
+++ src/main/java/com/example/finalproject_oop213/HelloApplication.java
@@ -17,9 +17,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                HelloApplication.class.getResource("/com/example/finalproject_oop213/MirzaMdSufianHridoy_fxml/ticketagent/TicketAgent-Dashboard-page.fxml"));
+                HelloApplication.class.getResource("AgentDashboard.fxml"));
 
-        Scene scene = new Scene(fxmlLoader.load(), 720, 740);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/com/example/finalproject_oop213/IsratJahan/FinancialOfficerDashboardController.java
@@ -0,0 +1,46 @@
+package com.example.finalproject_oop213.IsratJahan;
+
+import javafx.event.ActionEvent;
+
+public class FinancialOfficerDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void goal7Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal1Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal6Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal2Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal4Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal8Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal5Button(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void goal3Button(ActionEvent actionEvent) {
+    }
+}
